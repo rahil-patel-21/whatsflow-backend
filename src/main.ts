60377ef6b0f982b5b14d7dc1372c40e76bcfda16
TechAddict <rahil.311298@gmail.com>
2025-04-20 10:00:04 +0530

    Changes in port

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docBook', app, document);
 
-  await app.listen(3000);
+  await app.listen(3007);
 }
 
 bootstrap();
